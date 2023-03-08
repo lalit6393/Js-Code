2e1e3a2b1d8dc55b4e6d9a5c6ac334feee139845
lalit6393 <amanengineer132@gmail.com>
2023-03-08 16:34:17 +0530

    code

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 const data = require("./data.json");
 
+//making promises so that code run asynchronously
+
+//promise to sort array according to percentage in 2019
+
 let Performer2019 = new Promise((resolve, reject) =>
   resolve(data.sort((a, b) => parseInt(b[2019]) - parseInt(a[2019])))
 );
 
+//promise to sort array according to percentage in 2024
+
 let Performer2024 = new Promise((resolve, reject) =>
   resolve(data.sort((a, b) => parseInt(b[2024]) - parseInt(a[2024])))
 );
+
+//promise to sort array according to percentage growth
 
 let growth = new Promise((resolve, reject) =>
   resolve(
@@ -18,6 +26,8 @@ let growth = new Promise((resolve, reject) =>
     )
   )
 );
+
+//first promise resolves
 
 Performer2019.then((data) => {
   let dataSize = data.length;
@@ -34,6 +44,8 @@ Performer2019.then((data) => {
     console.log("   " + (i + 1) + ". " + data[i].skills);
 }).catch(console.log);
 
+//Second promise resolves
+
 Performer2024.then((data) => {
   let dataSize = data.length;
   console.log("Rank in 2024: ");
@@ -48,6 +60,8 @@ Performer2024.then((data) => {
   for (let i = dataSize - 1; i > dataSize - 4; i--)
     console.log("   " + (i + 1) + ". " + data[i].skills);
 }).catch(console.log);
+
+//Third promise resolves
 
 growth
   .then((data) => {
